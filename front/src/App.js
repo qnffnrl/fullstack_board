ade0741014c8a23845b99ab986a80f38ad52d850
riskerGoldClass <roakfwk30@naver.com>
2023-01-09 01:45:58 +0900

    add : 게시글 상세 페이지 / 수정, 삭제 틀만 구현

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,12 @@
 import { Route, Routes } from "react-router-dom";
+import './App.css';
 import Nav from "./conponent/Nav";
 import Home from "./conponent/Home";
 import About from "./conponent/About"
 import Board from "./conponent/Board";
-import './App.css';
+import PostDetail from "./conponent/PostDetail";
+import Edit from "./conponent/Edit";
+import New from "./conponent/New";
 
 function App() {
   return (
@@ -13,6 +16,10 @@ function App() {
               <Route exact path="/" element={<Home/>}/>
               <Route exact path="/Board" element={<Board/>}/>
               <Route exact path="/About" element={<About/>}/>
+
+              <Route exact path="/Posts/:id" element={<PostDetail/>}/> {/* 게시물 상세보기 */}
+              <Route exact path="/Posts/:id/edit" element={<Edit/>}/>  {/* 게시물 수정 페이지 */}
+              <Route exact path="/Posts/new" element={<New/>}/>        {/* 게시물 작성 페이지 */}
           </Routes>
       </div>
   );
