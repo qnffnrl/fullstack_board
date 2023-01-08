8a8a794dd7cfb2f2854ff20518f9a277238ddb21
riskerGoldClass <roakfwk30@naver.com>
2023-01-08 17:46:53 +0900

    add : front router 구현 및 게시판 글 불러오기

--- front/src/App.js
+++ front/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import { Route, Routes } from "react-router-dom";
+import Nav from "./conponent/Nav";
+import Home from "./conponent/Home";
+import About from "./conponent/About"
+import Board from "./conponent/Board";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="page">
+          <Nav></Nav>
+          <Routes>
+              <Route exact path="/" element={<Home/>}/>
+              <Route exact path="/Board" element={<Board/>}/>
+              <Route exact path="/About" element={<About/>}/>
+          </Routes>
+      </div>
   );
 }
 
