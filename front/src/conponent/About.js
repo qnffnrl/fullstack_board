1bab7c8ac40c3c19973b1ddcef3365801261db62
riskerGoldClass <roakfwk30@naver.com>
2023-02-20 22:16:12 +0900

    add : About 페이지 로직흐름 사진 추가

--- front/src/conponent/About.js
+++ front/src/conponent/About.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 function About(props) {
     return (
-        <div>
-            This is About
+        <div className="d-flex justify-content-center">
+            <img id="about-image" src="images/logic.jpg"/>
         </div>
     );
 }
